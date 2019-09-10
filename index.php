88d88c5b1c3bd8a4c60cba19e32005ddb520b94e
JDS <scruffy403@gmail.com>
2019-09-10 14:23:37 +0100

    Test connection with test table

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php
+$connection = mysqli_connect("localhost", "root", "", "social");
+
+if(mysqli_connect_errno())
+{
+  echo "Failed to connect: " . mysqli_connect_errno();
+}
+
+$query = mysqli_query($connection, "INSERT INTO test VALUES(null, 'John')");
+
+ ?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>

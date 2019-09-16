7b9bc2ba60f63420f1039039f3faab7ed5efd64a
JDS <scruffy403@gmail.com>
2019-09-16 21:09:51 +0100

    Clean up code in index.php and register.php

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
-$connection = mysqli_connect("localhost", "root", "", "social");
-
-if(mysqli_connect_errno())
-{
-  echo "Failed to connect: " . mysqli_connect_errno();
-}
-
-$query = mysqli_query($connection, "INSERT INTO test VALUES(null, 'John')");
+require 'config/config.php';
 
  ?>
 

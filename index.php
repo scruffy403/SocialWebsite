336515d3b440ac2502d74a177e8a959b8193ec2c
JDS <scruffy403@gmail.com>
2019-09-21 23:30:04 +0100

    Create newsfeed column

--- index.php
+++ index.php
@@ -19,6 +19,18 @@ include("includes/header.php");
 
     </div>
 
+    <div class="main_column column">
+
+      <form class="post_form" action="index.php" method="POST">
+        <textarea name="post_text" id="post_text" placeholder="Got something to say?"></textarea>
+        <input type="submit" name="post" id="post_button" value="Post">
+        <hr>
+
+      </form>
+
+
+    </div>
+
   </div>
   </body>
 </html>

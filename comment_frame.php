072a78d0d9ae5499917bb37e7eda4720c0531a97
JDS <scruffy403@gmail.com>
2019-10-18 22:19:49 +0100

    Style comments on posts

--- comment_frame.php
+++ comment_frame.php
@@ -1,3 +1,20 @@
+<?php
+require 'config/config.php';
+include("includes/classes/User.php");
+include("includes/classes/Post.php");
+
+if(isset($_SESSION['username']))
+{
+  $userLoggedIn = $_SESSION['username'];
+  $user_details_query = mysqli_query($connection, "SELECT * FROM users WHERE username='$userLoggedIn'");
+  $user = mysqli_fetch_array($user_details_query);
+}
+else {
+  header("Location: register.php");
+}
+
+ ?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -6,22 +23,15 @@
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
   </head>
   <body>
-    <?php
-    require 'config/config.php';
-    include("includes/classes/User.php");
-    include("includes/classes/Post.php");
-
-    if(isset($_SESSION['username']))
-    {
-      $userLoggedIn = $_SESSION['username'];
-      $user_details_query = mysqli_query($connection, "SELECT * FROM users WHERE username='$userLoggedIn'");
-      $user = mysqli_fetch_array($user_details_query);
-    }
-    else {
-      header("Location: register.php");
+    <style media="screen">
+    * {
+      font-size: 12px;
+      font-family: Arial, Helvetica, sans-serif;
     }
 
-     ?>
+
+    </style>
+
      <script type="text/javascript">
       function toggle() {
         var element = document.getElementById("comment_section");

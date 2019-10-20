45b29f035a25d4959a6f6980ba6b6af33d1dc552
JDS <scruffy403@gmail.com>
2019-10-20 22:06:28 +0100

    Make and style left profile bar

--- profile.php
+++ profile.php
@@ -1,9 +1,35 @@
 <?php
 include("includes/header.php");
+
+if (isset($_GET['profile_username'])) {
+  $username = $_GET['profile_username'];
+  $user_details_query = mysqli_query($connection, "SELECT * FROM users WHERE username='$username'");
+  $user_array = mysqli_fetch_array($user_details_query);
+
+  $number_of_friends = (substr_count($user_array['friend_array'], ",")) -1;
+}
  ?>
 
+    <style media="screen">
+      .wrapper {
+        margin-left: 0px;
+        padding-left: 0px;
+      }
+    </style>
+
+    <div class="profile_left">
+      <img src="<?php echo $user_array['profile_pic']; ?>" alt="profile picture">
+
+      <div class="profile_info">
+        <p><?php echo "Posts: " . $user_array['num_posts']; ?></p>
+        <p><?php echo "Likes: " . $user_array['num_likes']; ?></p>
+        <p><?php echo "Friends: " . $number_of_friends; ?></p>
+
+      </div>
+    </div>
+
     <div class="main_column column">
-      This is a profile page!
+      <?php echo $username; ?>
 
     </div>
 

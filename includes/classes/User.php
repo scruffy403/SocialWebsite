b2bca085fe2163e84602c9453cb4b519d7f13cd2
JDS <scruffy403@gmail.com>
2019-10-24 13:10:30 +0100

    Add remove friend functionality

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -91,6 +91,20 @@ class User {
     }
   }
 
+  public function removeFriend($user_to_remove) {
+    $logged_in_user = $this->user['username'];
+
+    $query = mysqli_query($this->connection, "SELECT friend_array FROM users WHERE username='$user_to_remove'");
+    $row = mysqli_fetch_array($query);
+    $friend_array_username = $row['friend_array'];
+
+    $new_friend_array = str_replace($user_to_remove . ",", "", $this->user['friend_array']);
+    $remove_friend = mysqli_query($this->connection, "UPDATE users SET friend_array='$new_friend_array' WHERE username='$logged_in_user'");
+
+    $new_friend_array = str_replace($this->user['username'] . ",", "", $friend_array_username);
+    $remove_friend = mysqli_query($this->connection, "UPDATE users SET friend_array='$new_friend_array' WHERE username='$user_to_remove'");
+  }
+
 
 }
 

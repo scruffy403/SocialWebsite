296810f52759dff61236f4eaa151d5d193d020cc
JDS <scruffy403@gmail.com>
2019-10-30 15:55:04 +0000

    Make delete post button

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -110,6 +110,13 @@ class Post {
             $count ++;
           }
 
+          if ($userLoggedIn == $added_by) {
+            $delete_button = "<button class='delete_button btn-danger'id='post$id'>X</button>";
+          }
+          else {
+            $delete_button = "";
+          }
+
           $user_details_query = mysqli_query($this->connection, "SELECT first_name,
             last_name, profile_pic FROM users WHERE username='$added_by'");
           $user_row = mysqli_fetch_array($user_details_query);
@@ -215,6 +222,7 @@ class Post {
                         <div class='posted_by' style='color:#ACACAC;'>
                           <a href='$added_by'> $first_name $last_name
                           </a> $user_to &nbsp;&nbsp;&nbsp;&nbsp; $time_message
+                          $delete_button
                         </div>
                         <div id='post_body'>
                           $body
@@ -238,7 +246,28 @@ class Post {
                       <hr>";
         }
 
+        ?>
 
+        <script type="text/javascript">
+
+        $(document).ready(function(){
+
+          $('#post<?php $id;?>').on('click', function(){
+            bootbox.confirm("Are you sure you want to delete this post?", function(result) {
+
+              $.post("includes/form_handlers/delete_post.php?post_id=<?php echo $id; ?>", {result:result});
+
+              if(result)
+                location.reload();
+
+            });
+          });
+
+        });
+
+        </script>
+
+        <?php
 
       } // End of while loop
 

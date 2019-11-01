87a9e427d625b74087732a5af9731c2a7b3c748a
JDS <scruffy403@gmail.com>
2019-11-01 09:07:28 +0000

    Add mutual friends calculation

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -118,6 +118,26 @@ class User {
     $query = mysqli_query($this->connection, "INSERT INTO friend_requests VALUES(NULL, '$user_to', '$user_from')");
   }
 
+  public function getMutualFriends($username_to_check) {
+    $mutualFriends = 0;
+    $user_array = $this->user['friend_array'];
+    $user_array_explode = explode(",", $user_array);
+
+    $query = mysqli_query($this->connection, "SELECT friend_array FROM users WHERE username='$username_to_check'");
+    $row = mysqli_fetch_array($query);
+    $user_to_check_array = $row['friend_array'];
+    $user_to_check_array_explode = explode(",", $user_to_check_array);
+
+    foreach ($user_array_explode as $i) {
+      foreach ($user_to_check_array_explode as $j) {
+        if ($i == $j && $i != "") {
+          $mutualFriends++;
+        }
+      }
+    }
+    return $mutualFriends;
+  }
+
 
 }
 
